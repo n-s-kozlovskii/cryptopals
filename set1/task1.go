48a78dab1941ddfbe01bc194f6d2c653c9087878
N K <n.s.kozlovskii@gmail.com>
2025-02-21 12:18:56 +0400

    task1 done

--- set1/task1.go
+++ set1/task1.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	b64 "encoding/base64"
+	"encoding/hex"
+	"fmt"
+	"log"
+)
+
+func HexToBase64(src []byte) (string, error) {
+	dst := make([]byte, hex.DecodedLen(len(src)))
+	n, err := hex.Decode(dst, src)
+	if err != nil {
+		return "", err
+	}
+	fmt.Printf("%s\n", dst[:n])
+
+	sEnc := b64.StdEncoding.EncodeToString(dst[:n])
+	return sEnc, nil
+}
 
 func main() {
-	fmt.Println("hello world")
+	src := []byte("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
+
+	sEnc, err := HexToBase64(src)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(sEnc)
 }
