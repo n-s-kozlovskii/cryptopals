2edee881508cb5986563ed2e00304992aefc3e5c
N K <n.s.kozlovskii@gmail.com>
2025-02-23 14:19:36 +0400

    add scoring

--- set1/task3.go
+++ set1/task3.go
@@ -3,6 +3,7 @@ package set1
 import (
 	"fmt"
 	"log"
+	"strings"
 )
 
 func FixedXor2(left, right []byte) (string, error) {
@@ -25,11 +26,12 @@ func FillToLength(n int, b byte) []byte {
 
 	return res
 }
-func Decypher(input string) []string {
+func Decypher(input string) string {
 	alphabet := "ETAONRISHDLFCMUGYPWBVKJXZQ"
 	dst := LiteralToHex(input)
 	n := len(dst)
-	res := make([]string, len(alphabet))
+	var bestDecypher string
+	bestScore := -1
 
 	for i := 0; i < len(alphabet); i++ {
 		data := FillToLength(n, alphabet[i])
@@ -38,8 +40,51 @@ func Decypher(input string) []string {
 			fmt.Println("FixedXor(dst[:n], data) failed")
 			log.Fatal(err)
 		}
-		res[i] = string(xor)
+		score := Score(string(xor))
+		if score > bestScore {
+			bestDecypher = string(xor)
+			bestScore = score
+		}
 	}
-	fmt.Printf("task3 message: %s\n", res[23])
-	return res
+	fmt.Printf("task3 message: %s\n", bestDecypher)
+	return bestDecypher
+}
+
+func Score(a string) int {
+	a = strings.ToUpper(a)
+	table := map[rune]int{
+		'E': 28,
+		'T': 27,
+		'A': 26,
+		'O': 25,
+		'I': 24,
+		'N': 23,
+		'S': 22,
+		'R': 21,
+		'H': 20,
+		'D': 17,
+		'L': 16,
+		'U': 15,
+		'C': 14,
+		'M': 13,
+		'F': 12,
+		'Y': 11,
+		'W': 10,
+		'G': 9,
+		'P': 8,
+		'B': 7,
+		'V': 6,
+		'K': 5,
+		'X': 4,
+		'Q': 3,
+		'J': 2,
+		'Z': 1,
+		' ': 1,
+	}
+	var score int
+	for _, ch := range a {
+		score += table[ch]
+	}
+
+	return score
 }
--- set1/task3_test.go
+++ set1/task3_test.go
@@ -7,11 +7,7 @@ import (
 func TestDecypher(t *testing.T) {
 	want := "Cooking MC's like a pound of bacon"
 	got := Decypher("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
-	found := false
-	for _, decyphered := range got {
-		found = found || decyphered == want
-	}
-	if !found {
+	if got != want {
 		t.Errorf("Decypher() = %q, want %q", got, want)
 	}
 }
